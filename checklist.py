38c1fa36cb37fb05c4e8a18aa4e11602f057885a
Justin <149697739+Freedoough@users.noreply.github.com>
2025-04-21 14:12:04 -0400

    Create checklist.py

---
+++ checklist.py
@@ -0,0 +1,13 @@
+from logging import root
+import tkinter as tk
+from tkinter import ttk
+import math
+import time
+
+
+
+root = tk.Tk()
+root.title("Checklist")
+
+# Create a frame for the checklist
+root.geometry("400x400")
