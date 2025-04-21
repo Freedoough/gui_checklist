75e5351c2710c517b751472a80bb1dd4c61dc3a8
Justin <149697739+Freedoough@users.noreply.github.com>
2025-04-21 16:56:53 -0400

    labelling and window configuration. change from .pack to .grid method

--- checklist.py
+++ checklist.py
@@ -11,6 +11,17 @@ root.title("Checklist")
 root.geometry("500x800") 
 
 
+# # Create label for task name entry box
+# task_name_label = tk.Label(root, text="Task Name:", font="Arial, 12")
+# task_name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+# # Create an entry box for task name
+# entry_box = tk.Entry(root, width=50, justify="center")
+# entry_box.grid(row=0, column=1, padx=5, pady=5)
+
+
+
+
 
 # Create a label for the title
 title_label = tk.Label(root, text="Checklist", font=("Arial, 24")) 
@@ -21,8 +32,16 @@ title_label.pack(pady=10)
 task_name_label = tk.Label(root, text="Task Name:", font="Arial, 12")
 task_name_label.pack(pady=5, anchor="w")
 
-entry_box = tk.Entry(root, width=50)
-entry_box.pack(anchor="nw") 
+# Create an entry box for task name
+entry_box = tk.Entry(root, width=50, justify="center")
+entry_box.pack() 
+
+# Create a label for task entry box
+task_entry_label = tk.Label(root, text="Tasks" , font="Arial, 12")
+task_entry_label.pack(pady=5, anchor="w")
+
+
+
 
 
 # Create a list of items
