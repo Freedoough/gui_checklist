323a1b7f0e42148ad29e11e1cf714ba9218f7d8d
Justin <149697739+Freedoough@users.noreply.github.com>
2025-04-23 09:17:00 -0400

    main window configuration and widget placement

--- checklist.py
+++ checklist.py
@@ -8,44 +8,51 @@ import time
 # Root window setup
 root = tk.Tk()
 root.title("Checklist")
-root.geometry("500x800") 
-
-
-# # Create label for task name entry box
-# task_name_label = tk.Label(root, text="Task Name:", font="Arial, 12")
-# task_name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-
-# # Create an entry box for task name
-# entry_box = tk.Entry(root, width=50, justify="center")
-# entry_box.grid(row=0, column=1, padx=5, pady=5)
+root.geometry("700x600") 
 
 
 
 
 
-# Create a label for the title
-title_label = tk.Label(root, text="Checklist", font=("Arial, 24")) 
-title_label.pack(pady=10) 
+# Title label
+title_label = tk.Label(root, text="Checklist", font=("Arial, 24"), justify="center") 
+title_label.grid(row=0, column=1, pady=5) 
 
 
-# Create label for task name entry box
-task_name_label = tk.Label(root, text="Task Name:", font="Arial, 12")
-task_name_label.pack(pady=5, anchor="w")
+# Task name label next to entry box
+task_name_label = tk.Label(root, text="Task Name:", font="Arial, 12", justify="left")
+task_name_label.grid(row=3, column=0, padx=1, pady=5)
 
-# Create an entry box for task name
+
+# Entry box
 entry_box = tk.Entry(root, width=50, justify="center")
-entry_box.pack() 
-
-# Create a label for task entry box
-task_entry_label = tk.Label(root, text="Tasks" , font="Arial, 12")
-task_entry_label.pack(pady=5, anchor="w")
+entry_box.grid(row=3, column=1, padx=1, pady=5)
 
 
+# Listbox label
+task_entry_label = tk.Label(root, text="Tasks" , font="Arial, 12", justify="left")
+task_entry_label.grid(row=6, column=0, pady=3)
 
 
+# Listbox for entered tasks
+task_listbox = tk.Listbox(root, width=30, height=20, justify="left")
+task_listbox.grid(row=8, column=0, padx=6, pady=5)
 
-# Create a list of items
 
+# Completed tasks listbox label
+completed_tasks_label = tk.Label(root, text="Completed Tasks", font="Arial, 12", justify="right")
+completed_tasks_label.grid(row=6, column=2, pady=3)
+
+# Listbox for completed tasks
+completed_tasks_lebel = tk.Listbox(root, width=30, height=20, justify="right")
+completed_tasks_lebel.grid(row=8, column=2, padx=2, pady=5)
+
+
+# Clear all button widget
+
+clear_button = tk.Button(root, text="Clear all", font="Arial, 12", command=lambda: task_listbox.delete(0, tk.END), justify="center")
+    
+clear_button.grid(row=9, column=1, padx=0, pady=0)
 
 # Main program loop
 root.mainloop()
